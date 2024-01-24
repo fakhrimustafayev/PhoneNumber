facc69f6dc7ae0f9762f665061e8895b88252e7b
fakhrimustafayev <132089830+fakhrimustafayev@users.noreply.github.com>
2024-01-24 21:45:58 +0400

    Add files via upload

---
+++ PhoneNumber.sql
@@ -0,0 +1,37 @@
+﻿ALTER FUNCTION dbo.PhoneNumber
+(
+    @PhoneNumber NVARCHAR(20)
+)
+RETURNS NVARCHAR(20)
+AS
+BEGIN
+    DECLARE @FormattedNumber NVARCHAR(20)
+
+  
+        SET @PhoneNumber = REPLACE(@PhoneNumber, '+', '') -- '+' simvolu varsa təmizleyirik
+        SET @PhoneNumber = REPLACE(@PhoneNumber, ' ', '') -- boşluq varsa təmizləyirik
+
+    -- eger 0-9 arası reqemlerden kenar başqa simvol yazılarsa NULL qaytarsın
+  IF @PhoneNumber LIKE '%[^0-9]%'
+BEGIN
+    RETURN NULL
+END
+
+    -- Ən az 9 rəqəm olmalıdır
+    IF LEN(@PhoneNumber) < 9
+    BEGIN
+        -- 9dan az reqem varsa NULL döndür
+        RETURN NULL
+    END
+
+    -- +994 elevə ederek nömreni düzeldirik
+    SET @FormattedNumber = '+994 (' + SUBSTRING(@PhoneNumber, LEN(@PhoneNumber) - 9, 3) + ') ' +
+                          SUBSTRING(@PhoneNumber, LEN(@PhoneNumber) - 6, 3) + ' ' +
+                          SUBSTRING(@PhoneNumber, LEN(@PhoneNumber) - 3, 2) + ' ' +
+                          SUBSTRING(@PhoneNumber, LEN(@PhoneNumber) - 1, 2)
+
+    RETURN @FormattedNumber
+END
+
+
+
---
+++ PhoneNumberYoxlama.sql
@@ -0,0 +1,14 @@
+﻿SELECT dbo.PhoneNumber('0503421296') AS FormattedNumber
+-- Çıktı: +994 (050) 342 12 96
+
+SELECT dbo.PhoneNumber('505643278') AS FormattedNumber
+-- Çıktı: +994 (50) 564 32 72
+
+SELECT dbo.PhoneNumber('9940501588448') AS FormattedNumber
+-- Çıktı: +994 (050) 158 84 48
+
+SELECT dbo.PhoneNumber('+9940504761966') AS FormattedNumber
+-- Çıktı: +994 (050) 476 19 66
+
+SELECT dbo.PhoneNumber('123hewr56789') AS FormattedNumber
+-- Çıktı: NULL (Geçersiz telefon numarası)
